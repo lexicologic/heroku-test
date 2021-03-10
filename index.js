6d1a045bd86b6f9fe36e0c8bcf26c00c16bac515
Natalie Guidry <nguidry7@gmail.com>
2021-03-10 17:22:41 -0600

    added npm install to fix build

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Dependencies
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json(path.join(__dirname, "index.html"));
+  const html = fs.readFile(path.join(__dirname, "index.html"), (e, data) => {
+    if (e) console.error(e);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  })
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
